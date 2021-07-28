54e0af0aea6aee38e6f87be03f248723a1ced20d
Hugues Odegaard <hugues.odegaard@openairlines.com>
2021-07-28 17:11:07 +0200

    add image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterContentInit, AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterContentInit, AfterViewInit, Component, Host, HostBinding, OnInit, ViewChild} from '@angular/core';
 import {AudioPlayerComponent, Track} from 'ngx-audio-player';
 
 @Component({
@@ -9,6 +9,8 @@ import {AudioPlayerComponent, Track} from 'ngx-audio-player';
 export class AppComponent {
   title = 'clemence-audio';
   
+  // @HostBinding() class = 'h-100';
+  
   @ViewChild('audioPlayer', { static: true })
   audioPlayer?: AudioPlayerComponent;
   
@@ -18,7 +20,8 @@ export class AppComponent {
       title: 'In Love',
       link:
           'https://dl.dropboxusercontent.com/s/9v0psowra7ekhxo/A%20Himitsu%20-%20In%20Love%20%28feat.%20Nori%29.flac?dl=0',
-      artist: 'A Himitsu feat. Nori'
+      artist: 'A Himitsu feat. Nori',
+      duration: 227
     }
   ];
 }
